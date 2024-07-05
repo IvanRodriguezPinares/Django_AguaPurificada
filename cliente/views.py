252aa0cdd53093ff1fdc993bd23d5cb23bdd1b4e
IvanRodriguezPinares <iv.rodriguezp@duocuc.cl>
2024-07-05 03:02:32 -0400

    Conectamos indexBefine.html al proyecto

--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request): #este index llamo en urls.py de cliente
+    
+    return render(request,'cliente/indexBefine.html') #dirección de donde esta mi index
